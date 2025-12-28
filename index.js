27c2a5acc0723959aa372a16459b7623767316bd
Zaid <mdzaid2810@gmail.com>
2025-12-28 19:59:43 +0530

    feat:add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("This is a test file.");
+console.log("It is used for CI/CD pipeline testing.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Starting test script...');
+setTimeout(() => {
+    console.log('Test script completed.');
+}, 5000);
+console.log('This is a test completed.');
